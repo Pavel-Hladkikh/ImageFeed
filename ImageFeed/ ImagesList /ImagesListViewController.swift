e1eadb6873de9d27bd24cee267f7ff72d42542c3
Pavel Hladkikh <hladkikh94@gmail.com>
2025-07-22 19:05:45 +0200

    Sprint 09 завершен

--- ImageFeed/ ImagesList /ImagesListViewController.swift
+++ ImageFeed/ ImagesList /ImagesListViewController.swift
@@ -2,7 +2,7 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
-    
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ru_RU")
@@ -21,6 +21,23 @@ final class ImagesListViewController: UIViewController {
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
         tableView.dataSource = self
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+
     
     private func configCell(for cell: ImagesListCell,
                             with indexPath: IndexPath) {
@@ -38,7 +55,9 @@ final class ImagesListViewController: UIViewController {
     }
 }
 extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
@@ -71,3 +90,4 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
 }
+
---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,130 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 35
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let logoutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "logout_button"), for: .normal)
+        button.tintColor = .systemRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 23, weight: .semibold)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let loginNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        view.backgroundColor = UIColor(
+            red:   26.0/255.0,
+            green: 27.0/255.0,
+            blue:  34.0/255.0,
+            alpha: 1.0
+        )
+
+        setupSubviews()
+        setupConstraints()
+        setupActions()
+        configureData()
+    }
+
+
+    private func setupSubviews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(logoutButton)
+        view.addSubview(nameLabel)
+        view.addSubview(loginNameLabel)
+        view.addSubview(descriptionLabel)
+    }
+
+
+
+    private func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+               // 1) Аватарка
+               avatarImageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 32),
+               avatarImageView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+               avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+               avatarImageView.heightAnchor.constraint(equalToConstant: 70),
+
+               logoutButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+               logoutButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+               logoutButton.widthAnchor.constraint(equalToConstant: 44),
+               logoutButton.heightAnchor.constraint(equalToConstant: 44),
+
+               nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+               nameLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+               nameLabel.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
+
+               loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+               loginNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+               loginNameLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+
+               descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+               descriptionLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+               descriptionLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+           ])
+    }
+
+    
+
+    private func setupActions() {
+        logoutButton.addTarget(
+            self,
+            action: #selector(didTapLogoutButton),
+            for: .touchUpInside
+        )
+    }
+
+    @objc
+    private func didTapLogoutButton() {
+        // просто выводим сообщение в консоль
+        print("Кнопку Logout нажали")
+    }
+
+    
+
+    private func configureData() {
+        avatarImageView.image = UIImage(named: "avatar")
+        nameLabel.text = "Екатерина Новикова"
+        loginNameLabel.text = "@ekaterina_nov"
+        descriptionLabel.text = "Hello, World!"
+    }
+
+}
